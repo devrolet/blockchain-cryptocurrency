f5946353653ce5e58de389a560a8a9ebe195adcd
Chet Hill <chetdhill@gmail.com>
2018-04-30 20:34:35 -0400

    Genesis Block

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,10 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'zoo', 'maz');
-console.log(block.toString());
+//
+// const block = new Block('foo', 'bar', 'zoo', 'maz');
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
+
+//Test Mine Hash
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
